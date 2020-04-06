bf45c1104fd5c415773c83227f01872f69cdde27
fsteppich <fsteppich@gmail.com>
2020-04-06 02:46:33 +0200

    Setup initial package structure
    Setup the initial package structure for the FSteppich Data Science
    Utility package. This package is intended to make work as a Data
    Scientist more fun by reducing the amount of repetitive work.
    
    Resolves: -
    See also: -

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='fs_ds_util',
+      version='0.1',
+      description='FSteppich Data Science Utilities',
+      packages=['fs_ds_util'],
+      zip_safe=False)
